7b16625e2e5955c6b8443bd345b2be929c211fb5
new-web-apps <web-applications@yandex.ru>
2025-02-03 15:45:56 +0300

    inpur signel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CardComponent } from './card/card.component';
 import { CommonModule } from '@angular/common';
 import { MatGridListModule } from '@angular/material/grid-list';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { NavbarComponent } from './navbar/navbar.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CardComponent, CommonModule, MatGridListModule, MatSidenavModule, NavbarComponent],
+  imports: [RouterOutlet, CardComponent, CommonModule, MatGridListModule, MatSidenavModule, NavbarComponent, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-eslint';
+  readonly isOpen = signal(false);
 
   cardContent = [
     {
@@ -38,4 +40,8 @@ export class AppComponent {
       text: 'Test test test test test test',
     },
   ];
+
+  openMenu() {
+    this.isOpen.update(value => !value);
+  }
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatSidenavModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+  isOpen = input(true)
 }
